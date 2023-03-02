5146b3bb11f9fc840309c7c134821868b36f57ad
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-03-02 11:11:52 +0200

    Add files via upload

---
+++ Notepad/Notepad/GoToForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Notepad
+{
+    partial class Перейти
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLineNum = new System.Windows.Forms.NumericUpDown();
+            this.butGo = new System.Windows.Forms.Button();
+            this.labGo = new System.Windows.Forms.Label();
+            this.butCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tbLineNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbLineNum
+            // 
+            this.tbLineNum.Location = new System.Drawing.Point(21, 44);
+            this.tbLineNum.Name = "tbLineNum";
+            this.tbLineNum.Size = new System.Drawing.Size(122, 20);
+            this.tbLineNum.TabIndex = 0;
+            // 
+            // butGo
+            // 
+            this.butGo.Location = new System.Drawing.Point(21, 70);
+            this.butGo.Name = "butGo";
+            this.butGo.Size = new System.Drawing.Size(57, 23);
+            this.butGo.TabIndex = 1;
+            this.butGo.Text = "button1";
+            this.butGo.UseVisualStyleBackColor = true;
+            // 
+            // labGo
+            // 
+            this.labGo.AutoSize = true;
+            this.labGo.Location = new System.Drawing.Point(18, 18);
+            this.labGo.Name = "labGo";
+            this.labGo.Size = new System.Drawing.Size(125, 13);
+            this.labGo.TabIndex = 3;
+            this.labGo.Text = "Введите номер строки:";
+            this.labGo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // butCancel
+            // 
+            this.butCancel.Location = new System.Drawing.Point(84, 70);
+            this.butCancel.Name = "butCancel";
+            this.butCancel.Size = new System.Drawing.Size(57, 23);
+            this.butCancel.TabIndex = 4;
+            this.butCancel.Text = "button2";
+            this.butCancel.UseVisualStyleBackColor = true;
+            // 
+            // Перейти
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(166, 111);
+            this.Controls.Add(this.butCancel);
+            this.Controls.Add(this.labGo);
+            this.Controls.Add(this.butGo);
+            this.Controls.Add(this.tbLineNum);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Перейти";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "GoToForm";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.tbLineNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button butGo;
+        private System.Windows.Forms.Label labGo;
+        private System.Windows.Forms.Button butCancel;
+        public System.Windows.Forms.NumericUpDown tbLineNum;
+    }
+}
---
+++ Notepad/Notepad/SearchForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Notepad
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.tbReplace = new System.Windows.Forms.TextBox();
+            this.cbReg = new System.Windows.Forms.CheckBox();
+            this.btFind = new System.Windows.Forms.Button();
+            this.btReplace = new System.Windows.Forms.Button();
+            this.btReplaceAll = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Найти:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Заменить на:";
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(93, 19);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(278, 20);
+            this.tbFind.TabIndex = 2;
+            // 
+            // tbReplace
+            // 
+            this.tbReplace.Location = new System.Drawing.Point(93, 44);
+            this.tbReplace.Name = "tbReplace";
+            this.tbReplace.Size = new System.Drawing.Size(278, 20);
+            this.tbReplace.TabIndex = 3;
+            // 
+            // cbReg
+            // 
+            this.cbReg.AutoSize = true;
+            this.cbReg.Location = new System.Drawing.Point(15, 70);
+            this.cbReg.Name = "cbReg";
+            this.cbReg.Size = new System.Drawing.Size(124, 17);
+            this.cbReg.TabIndex = 4;
+            this.cbReg.Text = "Учитывать регистр";
+            this.cbReg.UseVisualStyleBackColor = true;
+            // 
+            // btFind
+            // 
+            this.btFind.Location = new System.Drawing.Point(12, 93);
+            this.btFind.Name = "btFind";
+            this.btFind.Size = new System.Drawing.Size(86, 23);
+            this.btFind.TabIndex = 5;
+            this.btFind.Text = "Найти далее";
+            this.btFind.UseVisualStyleBackColor = true;
+            // 
+            // btReplace
+            // 
+            this.btReplace.Location = new System.Drawing.Point(104, 93);
+            this.btReplace.Name = "btReplace";
+            this.btReplace.Size = new System.Drawing.Size(86, 23);
+            this.btReplace.TabIndex = 6;
+            this.btReplace.Text = "Заменить";
+            this.btReplace.UseVisualStyleBackColor = true;
+            // 
+            // btReplaceAll
+            // 
+            this.btReplaceAll.Location = new System.Drawing.Point(196, 93);
+            this.btReplaceAll.Name = "btReplaceAll";
+            this.btReplaceAll.Size = new System.Drawing.Size(86, 23);
+            this.btReplaceAll.TabIndex = 7;
+            this.btReplaceAll.Text = "Заменить всё";
+            this.btReplaceAll.UseVisualStyleBackColor = true;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(288, 93);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(86, 23);
+            this.btCancel.TabIndex = 8;
+            this.btCancel.Text = "Отмена";
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 123);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btReplaceAll);
+            this.Controls.Add(this.btReplace);
+            this.Controls.Add(this.btFind);
+            this.Controls.Add(this.cbReg);
+            this.Controls.Add(this.tbReplace);
+            this.Controls.Add(this.tbFind);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SearchForm";
+            this.Text = "SearchForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox tbFind;
+        public System.Windows.Forms.TextBox tbReplace;
+        private System.Windows.Forms.CheckBox cbReg;
+        private System.Windows.Forms.Button btFind;
+        private System.Windows.Forms.Button btReplace;
+        private System.Windows.Forms.Button btReplaceAll;
+        private System.Windows.Forms.Button btCancel;
+    }
+}
